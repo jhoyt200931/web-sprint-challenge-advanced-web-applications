a38053d92ab646520418029e7115713c7b583385
Johnathan Hoyt <johnathanhoyt@Gabriellas-MacBook-Pro.local>
2020-10-09 23:14:11 -0400

    writing test

--- client/src/components/BubblePage.test.js
+++ client/src/components/BubblePage.test.js
@@ -1,7 +1,28 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import BubblePage from "./BubblePage";
+import {axiosWithAuth as mockAxios} from '../api/axiosWithAuth';
 
-test("Fetches data and renders the bubbles", () => {
+jest.mock('../api/axiosWithAuth');
+
+const mockColor = {
+  code: {
+    hex: '#7fffd4'
+  },
+  color: 'aqua',
+  id: 1
+}
+
+test("Fetches data and renders the bubbles", async () => {
   // Finish this test
+  mockAxios.mockResolvedValueOnce( mockColor );
+  const {getByText} = render(<BubblePage />)
+
+  await wait (() => {
+    getByText(/aqua/i);
+  });
+  const color = getByText(/aqua/i);
+
+  expect(color).toBeInTheDocument();
+
 });
